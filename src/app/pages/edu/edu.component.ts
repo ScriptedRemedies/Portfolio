3ff4c30a0521c3b62d428274877da652aa13118f
Sam <sbushe1@wgu.edu>
2025-04-27 14:46:36 -0700

    Created pages

---
+++ src/app/pages/edu/edu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edu',
+  imports: [],
+  templateUrl: './edu.component.html',
+  styleUrl: './edu.component.scss'
+})
+export class EduComponent {
+
+}
