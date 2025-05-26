91dbdfbdb81c58c9530c0931be9263862758aad2
Sam <sbushe1@wgu.edu>
2025-05-26 14:29:48 -0700

    Created and styled contact component

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
