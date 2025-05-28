d52a94bcb784d539f7d90c2132bc4817413e094f
Sam <sbushe1@wgu.edu>
2025-05-28 15:35:49 -0700

    Replaced nav with skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {AboutComponent} from './pages/about/about.component';
 import {HistoryComponent} from './pages/history/history.component';
-import {SkillsComponent} from './pages/skills/skills.component';
 import {ProjectsComponent} from './pages/projects/projects.component';
 import {ContactComponent} from './pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutComponent, HistoryComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, HistoryComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
