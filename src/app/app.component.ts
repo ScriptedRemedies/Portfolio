6cc2501106987d0ae8c84625622e908f0d195bab
Sam <sbushe1@wgu.edu>
2025-05-28 16:19:46 -0700

    Starting point of new site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {AboutComponent} from './pages/about/about.component';
-import {HistoryComponent} from './pages/history/history.component';
-import {ProjectsComponent} from './pages/projects/projects.component';
-import {ContactComponent} from './pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, AboutComponent, HistoryComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
